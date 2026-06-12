242d635dc8c6e1e530bd10340ff1ed465f871ff2
xiaoruan <xiaoruan@payvip3j.local>
2026-06-13 03:51:33 +0800

    add update time

--- run_forever.py
+++ run_forever.py
@@ -16,34 +16,40 @@ while True:
 
     except Exception as e:
         print("❌", e)
-        
+
     now = datetime.now()
 
-if now.hour == 23:
-    next_hour = now.replace(
-        minute=59,
-        second=0,
-        microsecond=0
-    )
+    if now.hour == 23:
 
-    if now.minute >= 59:
+        next_hour = now.replace(
+            minute=59,
+            second=0,
+            microsecond=0
+        )
+
+        if now.minute >= 59:
+            next_hour = (
+                now + timedelta(days=1)
+            ).replace(
+                hour=1,
+                minute=0,
+                second=0,
+                microsecond=0
+            )
+
+    else:
+
         next_hour = (
-            now + timedelta(days=1)
+            now + timedelta(hours=1)
         ).replace(
-            hour=1,
             minute=0,
             second=0,
             microsecond=0
         )
-else:
-    next_hour = (
-        now + timedelta(hours=1)
-    ).replace(
-        minute=0,
-        second=0,
-        microsecond=0
-    )
-    sleep_seconds = (next_hour - now).total_seconds()
+
+    sleep_seconds = (
+        next_hour - now
+    ).total_seconds()
 
     print("⏰ Next run:", next_hour)
 
