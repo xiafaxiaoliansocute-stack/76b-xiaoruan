a0f1e6bcb9a7c7dd243ec30a0b51c187744c3d55
xiaoruan <xiaoruan@payvip3j.local>
2026-06-18 08:13:19 +0800

    auto update

--- run_forever.py
+++ run_forever.py
@@ -7,30 +7,35 @@ while True:
     try:
         print("🚀 Run:", datetime.now())
 
-        subprocess.Popen([
-            "python3",
-            "/Users/xiaoruan/Desktop/76b-getdata/main.py"
-        ])
+        subprocess.run(
+            ["python3", "/Users/xiaoruan/Desktop/76b-getdata/main.py"],
+            check=True
+        )
+        subprocess.run(
+           ["python3", "/Users/xiaoruan/Desktop/76b-getdata/nn22.py"],
+            check=True
+        )
 
-        subprocess.Popen([
-            "python3",
-            "/Users/xiaoruan/Desktop/76b-getdata/nn22.py"
-        ])
 
-        print("✅ Started both scripts")
+
+        print("✅ Done")
 
     except Exception as e:
         print("❌", e)
 
     now = datetime.now()
 
-    next_hour = (now + timedelta(hours=1)).replace(
+    next_hour = (
+        now + timedelta(hours=1)
+    ).replace(
         minute=0,
         second=0,
         microsecond=0
     )
 
-    sleep_seconds = (next_hour - now).total_seconds()
+    sleep_seconds = (
+        next_hour - now
+    ).total_seconds()
 
     print("⏰ Next run:", next_hour)
 
