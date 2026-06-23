0047201603f4869476b5aa14f838d7df346cafe7
xiaoruan <xiaoruan@payvip3j.local>
2026-06-23 08:49:19 +0800

    add update time

--- nn22.py
+++ nn22.py
@@ -9,8 +9,8 @@ from zoneinfo import ZoneInfo
 # CONFIG
 # ======================
 
-ACCOUNT = "nn22"
-TOKEN = "ilv8fn0hxnth6ejkksppgkjpxij3exgt5eoapvfb"
+ACCOUNT = "xiaoruan16011"
+TOKEN = "sa0zgof995ttty1ofpz9f8lbklwaupmh0tvh2uc9"
 
 BASE_URL = "https://api6.o-9-d-4.com/api/backend/trpc/channel.effect"
 HOUR_URL = "https://api6.o-9-d-4.com/api/backend/trpc/channel.hourReportSum"
